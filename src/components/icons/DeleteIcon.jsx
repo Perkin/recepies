4a3295012a7f5546a54be5c70872ac18752cb640
Dmitriy Soskov <dmitriy.soskov@dostavista.ru>
2026-03-02 19:18:57 +0300

    Add SVG icons for edit and delete recipe actions

---
+++ src/components/icons/DeleteIcon.jsx
@@ -0,0 +1,10 @@
+export function DeleteIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" {...props}>
+      <path d="M3 6h18" />
+      <path d="M8 6V4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2" />
+      <path d="M6 6l1 14a1 1 0 0 0 1 .93h8a1 1 0 0 0 1-.93L18 6" />
+      <path d="M10 11v6M14 11v6" />
+    </svg>
+  )
+}
