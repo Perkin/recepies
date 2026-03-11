795d02e6aa09b11ce203754a384a5bd126fd74bf
Dmitriy Soskov <dmitriy.soskov@dostavista.ru>
2026-03-11 17:36:20 +0300

    Update queue button with toggle text and icons

---
+++ src/components/icons/QueueIcon.jsx
@@ -0,0 +1,8 @@
+export function QueueIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" {...props}>
+      <path d="M4 7h10M4 12h10M4 17h7" />
+      <path d="M17 10v8m-4-4h8" />
+    </svg>
+  )
+}
---
+++ src/components/icons/QueueRemoveIcon.jsx
@@ -0,0 +1,8 @@
+export function QueueRemoveIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" {...props}>
+      <path d="M4 7h10M4 12h10M4 17h7" />
+      <path d="M13 14h8" />
+    </svg>
+  )
+}
